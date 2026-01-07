271034569d2f3a75a0c975f789ada2207ad766fe
tony-ngok <tonyn1999@outlook.com>
2026-01-07 11:42:22 -0500

    首個发送 API

--- app/api/single_sends/route.js
+++ app/api/single_sends/route.js
@@ -1,17 +1,32 @@
 import { NextResponse } from "next/server"
 
-export async function GET() {
+export async function POST(request) {
   const client = require("@sendgrid/client")
   client.setApiKey(process.env.SENDGRID_API_KEY)
 
+  const request_data = await request.json()
+
+  const send_at = request_data.send_at || "1970-01-01T00:00:00Z"
+  const subject = request_data.subject || "Test"
+  const plain_content = request_data.plain_content || "test content"
   const list_id = process.env.crossmap_blogs_clife_prayer_contact_list_id
 
-  const req_body = {
-    url: "/v3/marketing/contacts/search",
-    method: "POST",
-    body: {
-      query: `CONTAINS(list_ids, '${list_id}')`
+  const data = {
+    name: "test_single_send",
+    send_at: send_at,
+    send_to: {
+      "list_ids": [list_id]
+    },
+    email_config: {
+      subject: subject,
+      plain_content: plain_content
     }
+  }
+
+  const req_body = {
+    url: "/v3/marketing/singlesends",
+    method: "POST",
+    body: data
   }
 
   try {
