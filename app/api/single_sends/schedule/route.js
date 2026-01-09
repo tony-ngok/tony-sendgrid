14a11c2e8777c2088995cc1b771d2dec511c512f
tony-ngok <tonyn1999@outlook.com>
2026-01-09 11:31:15 -0500

    根據不同時區各自單次發送

--- app/api/single_sends/schedule/route.js
+++ app/api/single_sends/schedule/route.js
@@ -2,61 +2,111 @@
 // https://www.twilio.com/docs/sendgrid/api-reference/single-sends/schedule-single-send
 
 import { NextResponse } from "next/server"
+import moment from "moment"
+import "moment-timezone"
+
+function getSendAt(timezone, newsletterSendTime, newsletterSendDate) {
+  let localNow = moment.tz(timezone)
+  let localDate = localNow.format("YYYY-MM-DD")
+  if (localDate < newsletterSendDate) {
+    localDate = newsletterSendDate
+  }
+  let localTime = localNow.format("HH:mm:ss")
+  let localDatetime = localDate + " " + localTime
+  let localSendAt = localDate + " " + newsletterSendTime
+
+  if (localSendAt < localDatetime) {
+    localNow.add(1, "days")
+    localDate = localNow.format("YYYY-MM-DD")
+    localSendAt = localDate + " " + newsletterSendTime
+  }
+  console.log(localSendAt, timezone)
+
+  let utcSendAt = moment.tz(localSendAt, timezone)
+  return utcSendAt.toISOString()
+}
 
 export async function POST(request) {
   const client = require("@sendgrid/client")
   client.setApiKey(process.env.SENDGRID_API_KEY)
 
   const request_data = await request.json()
-  const segment_id = request_data.segment_id || "74247496-2992-4822-abde-f9b246847d10"
+  // const segment_id = request_data.segment_id || "74247496-2992-4822-abde-f9b246847d10"
+  const list_id = process.env.crossmap_blogs_clife_prayer_contact_list_id
   const subject = request_data.subject || "Test"
   const html_content = request_data.html_content || "<div><div><span>test content</span></div></div>"
-  const send_at = request_data.send_at || "1970-01-01T00:00:00Z"
+  const send_date = request_data.send_date || "1970-01-01"
+  const send_time = request_data.send_time || "00:00:00"
 
   const cancel_id = process.env.crossmap_blogs_daily_devotional_sendgrid_unsubscribe_group_id
   const sender_id = process.env.crossmap_blogs_clife_prayer_sendgrid_sender_id
 
-  const data = {
-    name: "test_single_send",
-    send_to: {
-      segment_ids: [segment_id]
-    },
-    email_config: {
-      subject: subject,
-      html_content: html_content,
-      suppression_group_id: parseInt(cancel_id),
-      sender_id: parseInt(sender_id)
-    }
-  }
-
   try {
     const req_body = {
-      url: "/v3/marketing/singlesends",
-      method: "POST",
-      body: data
+      url: "/v3/marketing/segments",
+      method: "GET",
+      qs: {
+        parent_list_ids: list_id
+      }
     }
-    console.log(data)
 
     const [response, body] = await client.request(req_body)
     if (response.statusCode >= 400) {
       return NextResponse.json({ error: body }, { status: response.statusCode })
     }
 
-    const ssid = body.id
-    const req_body1 = {
-      url: `/v3/marketing/singlesends/${ssid}/schedule`,
-      method: "PUT",
-      body: {
-        send_at: send_at
+    const segments = body.results.filter(item => item.name.startsWith("DEV-"))
+    if (!segments?.length) {
+      return NextResponse.json({ error: "No segments" }, { status: 404 })
+    }
+
+    let return_data = []
+    for (const seg of segments) {
+      const timezone = seg.name.split("-")[2]
+      const send_at = getSendAt(timezone, send_time, send_date)
+
+      const data = {
+        name: `test_single_send-${timezone}-${send_at}`,
+        send_to: {
+          segment_ids: [seg.id]
+        },
+        email_config: {
+          subject: subject,
+          html_content: html_content,
+          suppression_group_id: parseInt(cancel_id),
+          sender_id: parseInt(sender_id)
+        }
       }
+
+      const req_body1 = {
+        url: "/v3/marketing/singlesends",
+        method: "POST",
+        body: data
+      }
+
+      const [response1, body1] = await client.request(req_body1)
+      if (response1.statusCode >= 400) {
+        return NextResponse.json({ error: body1 }, { status: response1.statusCode })
+      }
+
+      const ssid = body1.id
+      const req_body2 = {
+        url: `/v3/marketing/singlesends/${ssid}/schedule`,
+        method: "PUT",
+        body: {
+          send_at: send_at
+        }
+      }
+
+      const [response2, body2] = await client.request(req_body2)
+      if (response2.statusCode >= 400) {
+        return NextResponse.json({ error: body2 }, { status: response2.statusCode })
+      }
+
+      return_data.push(body2)
     }
 
-    const [response1, body1] = await client.request(req_body1)
-    if (response1.statusCode >= 400) {
-      return NextResponse.json({ error: body1 }, { status: response1.statusCode })
-    }
-
-    return NextResponse.json({ data: body1 }, { status: 200 })
+    return NextResponse.json({ data: return_data }, { status: 200 })
   } catch (error) {
     console.error("SendGrid API Error:", error)
     const status = error.code || 500
